ca1fad4e44471b64ba2a57030ccab2cd68b89a17
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2020-12-05 16:16:01 +0100

    Styling + settng layout for about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -22,22 +22,39 @@ const About = ({ navStatus, setNavStatus, offsetX, offsetY }) => {
         open={navStatus ? "translateX(0%)" : "translateX(-100%)"}
       />
       <Hamburger navStatus={navStatus} setNavStatus={setNavStatus} />
-      <BgImg variants={imgWorkAnim} src={moutainDayImg} alt="moutains" />
+
+      <TopContainer>
+        <BgImg variants={imgWorkAnim} src={moutainDayImg} alt="moutains" />
+      </TopContainer>
+      <BottomContainer></BottomContainer>
     </AboutStyled>
   );
 };
 
 const AboutStyled = styled(motion.div)`
-  min-height: 100vh;
-  background: lightblue;
+  min-height: 200vh;
+`;
+
+const TopContainer = styled.div`
+  width: 100%;
+  min-height: 50vh;
+  background: linear-gradient(180deg, #488db4 0%, #bbe9fa 100%);
+  position: relative;
+  z-index: 15;
+`;
+const BottomContainer = styled.div`
+  width: 100%;
+  height: 150vh;
+  background: #091219;
 `;
 
 const BgImg = styled(motion.img)`
-  position: fixed;
-  bottom: 20%;
-  z-index: 5;
-  width: 100%;
+  position: absolute;
+  left: -1%;
+  bottom: 50%;
+  max-width: 110%;
   pointer-events: none;
+  z-index: 20;
 `;
 
 export default About;
